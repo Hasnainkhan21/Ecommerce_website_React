5300fc45c9f4c7dda7bfb9c42add83405dea3752
naino <Hasnainkhan21>
2025-02-27 00:51:05 +0500

    shop section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
-import Home from './pages/Home'
+import Home from './pages/Home';
+import Shop from './pages/Shop';
 
 function App() {
  
@@ -11,6 +12,7 @@ function App() {
     <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>}></Route>
+      <Route path='/Shop' element={<Shop/>}></Route>
     </Routes>
     <Footer/>
     </BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Categories from '../Components/Categories'; // Import the Categories comp
 import { setProducts } from '../Redux/productSlice'; // Fixed import path (removed space)
 import { useDispatch, useSelector } from 'react-redux';
 import ProductCard from '../Components/ProductCard';
+import Shop from './Shop';
 
 const Home = () => {
     const dispatch = useDispatch();
@@ -15,6 +16,7 @@ const Home = () => {
     }, [dispatch]);
 
     return (
+        <div>
         <div className='bg-white mt-2 px-4 md:px-16 lg:px-24 py-8'>
             <div className='container mx-auto py-4 flex flex-col md:flex-row space-y-8 md:space-y-0 md:space-x-8'>
                 {/* Categories List Section */}
@@ -64,6 +66,8 @@ const Home = () => {
                 ))}
                 </div>
             </div>
+        </div>
+        <Shop />
         </div>
     );
 };
